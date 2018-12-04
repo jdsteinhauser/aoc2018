d5830c3e8c12748863e121ea57daf4a0be9ff1e0
Jason Steinhauser <jason.steinhauser@gmail.com>
2018-12-03 19:44:44 -0600

    Day 3 in full

--- day3.exs
+++ day3.exs
@@ -1,5 +1,3 @@
-file = File.stream!("day3.txt")
-
 parse_claim = fn line ->
   line
   |> String.split(~r/[#@,:x\n ]/, trim: true)
@@ -7,14 +5,35 @@ parse_claim = fn line ->
   |> Enum.map(& elem(&1,0))
 end  
 
-part1 = fn ->
-  file
+claims =
+  File.stream!("day3.txt")
   |> Enum.map(&parse_claim.(&1))
+
+grid = 
+  claims
   |> Enum.flat_map(fn [claim, left, top, width, height] -> 
-        for x <- left..left+width-1, y <- top..top+height-1, do: {Integer.to_string(claim), x, y} end)
+        for x <- left..left+width-1, y <- top..top+height-1, do: {claim, x, y} end)
   |> Enum.reduce(%{}, fn {claim, x, y}, acc -> Map.update(acc, {x,y}, [claim], &[claim | &1]) end)
-  # |> Enum.each(fn {{x, y}, claims} -> IO.puts "#{x},#{y}: #{claims}" end)
+
+part1 = fn ->
+  grid
   |> Enum.count(fn {_k,v} -> Enum.count(v) > 1 end)
 end
 
 IO.puts "Part 1: #{part1.()}"
+
+part2 = fn ->
+  singles = 
+    grid
+    |> Enum.filter(fn {_k,v} -> Enum.count(v) == 1 end)
+    |> Enum.group_by(fn {_k,[v]} -> v end, fn _ -> true end)
+    |> Enum.map(fn {k, v} -> {k, Enum.count(v, & &1)} end)
+    |> Map.new()
+
+  claims
+  |> Enum.find(fn [claim, _, _, w, h] -> Map.get(singles, claim) == w * h end)
+  |> hd()
+  |> Integer.to_string()
+end
+
+IO.puts "Part 2: #{part2.()}"
