9da49fa6ad72a0a1354ce0a8778f9fc91fab6520
Jason Steinhauser <jason.steinhauser@gmail.com>
2018-12-06 21:15:59 -0600

    Day 6 finally complete

--- day6.exs
+++ day6.exs
@@ -7,14 +7,19 @@ manhattan = fn {x1, y1}, {x2, y2} -> abs(x2 - x1) + abs(y2 - y1) end
 
 coords = File.stream!("day6.txt") |> Enum.map(parse_coord)
 
-{xmin, xmax} = coords |> Enum.map(&elem(&1, 0)) |> Enum.minmax()
-{ymin, ymax} = coords |> Enum.map(&elem(&1, 1)) |> Enum.minmax()
-
-grid = for x <- xmin .. xmax, y <- ymin .. ymax do
-  %{coord: {x, y}, closest: coords |> Enum.sort_by(& manhattan.({x,y}, &1)) |> Enum.chunk_by(& manhattan.({x,y}, &1)) |> hd() }
-end
+{xmin, xmax} = coords |> Enum.map(&elem(&1, 0)) |> Enum.min_max()
+{ymin, ymax} = coords |> Enum.map(&elem(&1, 1)) |> Enum.min_max()
 
 part1 = fn ->
+  grid = for x <- xmin .. xmax, y <- ymin .. ymax do
+    %{coord: {x, y}, 
+      closest: coords
+              |> Enum.group_by(& manhattan.({x,y}, &1))
+              |> Enum.sort_by(fn {k, _v} -> k end)
+              |> Enum.map(fn {_k, v} -> v end)
+              |> hd() }
+  end
+
   edges = Enum.concat([
       Enum.filter(grid, fn %{coord: {x, _}} -> x == xmin end),
       Enum.filter(grid, fn %{coord: {x, _}} -> x == xmax end),
@@ -25,12 +30,21 @@ part1 = fn ->
     |> Enum.uniq()
 
   grid
-  |> Enum.flat_map(fn %{closest: xy} -> xy end)
+  |> Enum.reject(fn %{closest: xy} -> Enum.count(xy) != 1 end)
+  |> Enum.map(fn %{closest: [xy]} -> xy end)
   |> Enum.reject(& Enum.member?(edges, &1))
   |> Enum.group_by(& &1)
-  |> Enum.max_by(fn {_k, v} -> Enum.count(v) end)
-  |> (& elem(&1, 1)).()
-  |> Enum.count()
+  |> Enum.map(fn {_k, v} -> Enum.count(v) end)
+  |> Enum.max()
 end
 
 IO.puts "Part 1: #{part1.()}"
+
+part2 = fn ->
+  grid = for x <- xmin .. xmax, y <- ymin .. ymax, do: {x, y}
+  grid
+  |> Enum.map(fn xy -> Enum.map(coords, & manhattan.(&1, xy)) |> Enum.sum() end)
+  |> Enum.count(fn x -> x < 10000 end)
+end
+
+IO.puts "Part 2: #{part2.()}"
