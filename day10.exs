c6b23e9a721ad62fdb9d63237cefc5c6d10e0afe
Jason Steinhauser <jason.steinhauser@gmail.com>
2018-12-11 09:01:23 -0600

    Day 10 almost done...

--- day10.exs
+++ day10.exs
@@ -13,18 +13,26 @@ points = Enum.map(File.stream!("day10.txt"), parse_point)
 propagate = fn %{:pos => {x, y}, :vel => {vx, vy}} -> %{pos: { x + vx, y + vy }, vel: { vx, vy }} end
 
 print_points = fn points ->
-  {xmin, xmax} = Enum.map(points, fn %{:pos => {x, _}} -> x end) |> Enum.minmax()
-  {ymin, ymax} = Enum.map(points, fn %{:pos => {_, y}} -> y end) |> Enum.minmax()
+  {xmin, xmax} = Enum.map(points, fn %{:pos => {x, _}} -> x end) |> Enum.min_max()
+  {ymin, ymax} = Enum.map(points, fn %{:pos => {_, y}} -> y end) |> Enum.min_max()
 
   print_lines = List.duplicate(List.duplicate(?., xmax - xmin + 1) ++ '\n', ymax - ymin + 1)
-  points
-  |> Enum.reduce(print_lines, fn %{:pos => {x, y}}, lines -> List.update_at(lines, y - ymin, fn line -> List.replace_at(line, x - xmin, ?#) end) end)
+  Enum.reduce(points, print_lines, 
+              fn %{:pos => {x, y}}, lines -> 
+                  List.update_at(lines, y - ymin, 
+                  fn line -> List.replace_at(line, x - xmin, ?#) end) end)
 end
 
 score = fn points ->
-  
+  count = Enum.count(points)
+  dists = for a <- 0 .. count - 2, b <- a + 1 .. count - 1, do: manhattan.(Enum.at(points, a)[:pos], Enum.at(points, b)[:pos])
+  IO.puts Enum.sum(dists)
 end
 
 part1 = fn ->
-
+  states = Enum.scan(1..5, points, fn _, pts -> Enum.map(pts, propagate) end)
+  Enum.each(states, score)
+  List.last(states)
 end
+
+IO.puts "#{print_points.(part1.())}"
