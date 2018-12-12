16face026faa95a59756f30df4c4c2bf78aedd11
Jason Steinhauser <jason.steinhauser@gmail.com>
2018-12-11 21:40:19 -0600

    Day 8 done

--- day8.exs
+++ day8.exs
@@ -6,7 +6,7 @@ data =
 make_child = fn f, children, built_children, meta_count, numbers ->
   if children == Enum.count(built_children) do
     { meta, rest } = Enum.split(numbers, meta_count)
-    { built_children, meta, rest }
+    { Enum.reverse(built_children), meta, rest }
   else
     [child_count, meta_length | rest] = numbers
     { grandkids, meta, tail } = f.(f, child_count, [], meta_length, rest)
@@ -15,18 +15,16 @@ make_child = fn f, children, built_children, meta_count, numbers ->
 end
 
 calc_value = fn
-  f, 0 -> 0
-  f, [[], meta] -> Enum.sum(meta)
+  _f, 0 -> 0
+  _f, [[], meta] -> Enum.sum(meta)
   f, [children, meta] -> Enum.map(meta, fn x -> f.(f, Enum.at(children, x - 1, 0)) end) |> Enum.sum()
 end
 
-{tree, _, _} = make_child.(make_child, 1, [], 0, data)
+{[tree | _], _, _} = make_child.(make_child, 1, [], 0, data)
 
 part1 = List.flatten(tree) |> Enum.sum()
 
-part2 = fn ->
-
-end
+part2 = calc_value.(calc_value, tree)
 
 IO.puts "Part 1: #{part1}"
-IO.puts "Part 2: #{part2.()}"
+IO.puts "Part 2: #{part2}"
