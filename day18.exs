debfbceb56d3f3dfe8288e593bad521c38590bca
Jason Steinhauser <jason.steinhauser@gmail.com>
2018-12-19 20:20:19 -0600

    Day 18 complete

--- day18.exs
+++ day18.exs
@@ -2,13 +2,12 @@ mapping = %{?. => :open, ?| => :trees, ?# => :lumber }
 
 field = 
   File.stream!("day18.txt")
-  |> Enum.map(&String.rstrip/1)
-  |> Enum.map(&String.to_char_list/1)
+  |> Enum.map(&String.trim/1)
+  |> Enum.map(&String.to_charlist/1)
   |> Enum.map(fn line -> Enum.map(line, & mapping[&1]) |> Enum.with_index() end)
   |> Enum.with_index()
   |> Enum.flat_map(fn {list,y} -> Enum.map(list, fn {a, x} -> {[x,y], a} end) end)
-  |> Enum.reduce(%{}, fn {k,v}, acc -> Map.put_new(acc, k, v) end)
-  # |> Map.new()
+  |> Map.new()
 
 get_surroundings = fn field, [x,y] ->
   [[x-1, y-1], [x, y-1], [x+1, y-1],
@@ -17,8 +16,7 @@ get_surroundings = fn field, [x,y] ->
   |> Enum.map(&Map.get(field, &1, :nothing))
   |> Enum.group_by(& &1)
   |> Enum.map(fn {k, xs} -> {k, Enum.count(xs)} end)
-  |> Enum.reduce(%{}, fn {k,v}, acc -> Map.put_new(acc, k, v) end)
-  # |> Map.new()
+  |> Map.new()
 end
 
 update_acre = fn
@@ -47,12 +45,17 @@ end
 IO.inspect part1.()
 
 part2 = fn ->
-  Stream.iterate(field, update_field)
-  |> Stream.take(1000000001)
-  |> Enum.to_list()
-  |> List.last()
-  |> Enum.group_by(fn {_k, v} -> v end)
-  |> Enum.map(fn {k, xs} -> {k, Enum.count(xs)} end)
+  data =
+    field 
+    |> Stream.iterate(update_field)
+    |> Stream.map(fn f -> Enum.group_by(f, fn {_k, v} -> v end) |> Enum.map(fn {k, xs} -> {k, Enum.count(xs)} end) |> Map.new() end)
+    |> Stream.with_index()
+    |> Enum.take(1000)
+    |> Enum.reverse()
+  
+  # Series converges to a repeating sequence of 28 items
+  {%{:trees => trees, :lumber => lumber}, _idx} = Enum.find(data, fn {_vals, idx} -> rem(idx, 28) == rem(1_000_000_000, 28) end)
+  lumber * trees
 end
 
-IO.inspect part2.()
+IO.puts part2.()
